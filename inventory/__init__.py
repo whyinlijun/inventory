53972c3dfb74db8c989ac664b80a78c522ec3d0b
luke <whyinlijun@163.com>
2020-12-17 14:08:54 +0800

    _init__

--- inventory/__init__.py
+++ inventory/__init__.py
@@ -1,10 +1,16 @@
 import os
 from flask import Flask
+import click
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 db = SQLAlchemy(app)
+
+@click.command('say') #这里的字符串就是配合flask命令行要用到字符
+#@with_appcontext
+def say_hello():
+    click.echo('Initialized the database.')
 
 from inventory import views
 
